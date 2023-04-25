7cf72ff7013c2b0d99ae1f3fce7298a0149aa1ef
hdwmumu <keumhe0110@gmail.com>
2023-04-25 10:54:32 +0900

    완

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -6,16 +6,25 @@ const itemReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       //TODO
-
+      return Object.assign({}, state, {
+        cartItems: [...state.cartItems, action.payload]
+      })
       break;
+
     case REMOVE_FROM_CART:
       //TODO
-
+      let currentItem = state.cartItems.filter((el) => el.itemId !== action.payload.itemId)
+      return Object.assign({}, state, {
+        cartItems: currentItem
+      })
       break;
     case SET_QUANTITY:
       let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
       //TODO
-
+      return {
+        ...state,
+        cartItems: [...state.cartItems.slice(0, idx), action.payload, ...state.cartItems.slice(idx + 1)]
+      }
       break;
     default:
       return state;
